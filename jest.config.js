937f0fc5d22aea3072f031cb20d996daef476655
Ashley Qian <ashley.qian.0@gmail.com>
2023-02-22 05:41:21 +0000

    chore: remove jest.config.js and move into package.json

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testMatch: ['<rootDir>/tests/**/*.{spec,test}.{js,jsx,ts,tsx}'],
-  transform: {
-    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '@swc/jest',
-  },
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$'],
-};
